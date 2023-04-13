9a7f4548a7aebf7c99d39fec6935807241c39ddc
AgustinPalmaM <palma.agustin7@gmail.com>
2023-04-13 16:15:32 -0400

    Clean /routes/index.js we had some unusefull routes so i delete them

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,14 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send('index');
-});
+router.get('/', (req, res) => {
+  res.render('index')
+})
 
 router.get("/about", (req, res) => {
   
   const travels = 'Travel to Spain';
 
-
-  
   res.render('about', {
     travels
   });
@@ -21,19 +19,11 @@ router.get("/items", (req, res) => {
   
   const travels = 'Travel to Spain';
 
-
-  
   res.render('items', {
     travels
   });
 });
 
-router.get('/index', (req, res) => {
-  res.render('index')
-})
 
-router.get("/items", (req, res) => {
-  res.send('items');
-});
 
 export default router;

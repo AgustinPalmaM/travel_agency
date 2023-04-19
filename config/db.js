d7d7a35c87a8219e23b2a77363e3ae00fdb214b6
AgustinPalmaM <palma.agustin7@gmail.com>
2023-04-19 01:53:59 -0400

    Update again db config set operatorAliases to false

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 import dotenv from 'dotenv/config';
 
-// const op = Sequelize.Op;
+
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
@@ -16,7 +16,8 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     acquire: 30000,
     idle: 10000
   },
-  operatorAliases: true
+  operatorAliases: false
 });
 
 export default db;
+

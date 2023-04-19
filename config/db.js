656c9b0b36e216f8c3cea5c8b8679801ab320ee3
AgustinPalmaM <palma.agustin7@gmail.com>
2023-04-19 01:19:12 -0400

    Update db configuration

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 import dotenv from 'dotenv/config';
 
-
+const op = Sequelize.Op;
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
@@ -16,7 +16,15 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     acquire: 30000,
     idle: 10000
   },
-  operatorAliases: false
+  operatorAliases: {
+    $and: op.and,
+    $or: op.or,
+    $eq: op.eq,
+    $gt: op.gt,
+    $lt: op.lt,
+    $lte: op.lte,
+    $like: op.like
+}
 });
 
 export default db;

72ae24208d5dffb79225e815cc40f2911146d699
AgustinPalmaM <palma.agustin7@gmail.com>
2023-04-18 23:15:41 -0400

    Create a partial to manage testimonials render only in one file testimonials_block, so now into testimonials.pug and index.pug we only add include/layout/testimonials_block

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -1,74 +1,68 @@
-import { Travel } from "../models/Travel.js"
-import { Testimonial } from "../models/Testimonial.js"
+import { Travel } from "../models/Travel.js";
+import { Testimonial } from "../models/Testimonial.js";
 
-const indexPage = async(req, res) => {
-
+const indexPage = async (req, res) => {
   // make a query with 3 travels from the database
   try {
+    const travels = await Travel.findAll( { limit: 3 } );
+    const testimonials = await Testimonial.findAll( { limit: 3 } )
 
-    const travels = await Travel.findAll({limit: 3})
-
-    res.render('index', {
-      pagina: 'Index',
-      homeClass: 'home',
-      travels
-    })
+    res.render("index", {
+      pagina: "Index",
+      homeClass: "home",
+      travels,
+      testimonials
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
-}
+};
 
 const aboutPage = (req, res) => {
-  res.render('about', {
-    pagina: 'About Us'
-  })
-}
+  res.render("about", {
+    pagina: "About Us",
+  });
+};
 
 const travelsPage = async (req, res) => {
-
   const travels = await Travel.findAll();
 
-  res.render('travels', {
-    pagina: 'Next Trips',
-    travels
-  })
-}
+  res.render("travels", {
+    pagina: "Next Trips",
+    travels,
+  });
+};
 
 const detailTravelPage = async (req, res) => {
-  const { slug } =  req.params;
+  const { slug } = req.params;
 
   try {
-    const travel = await Travel.findOne({ where: { slug: slug }});
-     res.render('travel', {
-      pagina: 'Trip Information',
-      travel
-      
-     })
+    const travel = await Travel.findOne({ where: { slug: slug } });
+    res.render("travel", {
+      pagina: "Trip Information",
+      travel,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
-
-}
+};
 
 const testimonialsPage = async (req, res) => {
   try {
     const testimonials = await Testimonial.findAll();
-    res.render('testimonials', {
-      pagina: 'Testimonials',
-      testimonials
-    })
+    res.render("testimonials", {
+      pagina: "Testimonials",
+      testimonials,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 export {
   indexPage,
   aboutPage,
   travelsPage,
   testimonialsPage,
-  detailTravelPage
-}
-
+  detailTravelPage,
+};

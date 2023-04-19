f62a3531fa6429e1a43c60ac3ea84cfbf13b5d94
AgustinPalmaM <palma.agustin7@gmail.com>
2023-04-18 23:42:51 -0400

    Configure envienronment variables and data base config

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 import Sequelize from "sequelize";
+import dotenv from 'dotenv/config';
 
-const db = new Sequelize('your database name', 'your user', 'your password', {
-  host: 'your hostname here',
+
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   port: '3306',
   dialect: 'mysql',
   define: {
